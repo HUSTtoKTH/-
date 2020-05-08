2775e4e30119dcc787f34a7fce376f46be2b8b82
Wenhao <wenhaol@kth.se>
2020-05-08 18:26:06 +0200

    service

---
+++ src/main/java/org/seckill/dto/SeckillExecution.java
@@ -0,0 +1,27 @@
+package org.seckill.dto;
+
+import lombok.Data;
+import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStatEnum;
+
+@Data
+public class SeckillExecution {
+    private long seckillId;
+    private int state;
+    private String stateInfo;
+    private SuccessKilled successKilled;
+
+    public SeckillExecution(long seckillId, int state, String stateInfo) {
+        this.seckillId = seckillId;
+        this.state = state;
+        this.stateInfo = stateInfo;
+    }
+
+    public SeckillExecution(long seckillId, SeckillStatEnum stateEnum, SuccessKilled successKilled) {
+        this.seckillId = seckillId;
+        this.state = stateEnum.getState();
+        this.stateInfo = stateEnum.getStateInfo();
+        this.successKilled = successKilled;
+    }
+}
---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,56 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml",
+        "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private SeckillService seckillService;
+    @Test
+    public void getSeckillList() {
+        List<Seckill> list = seckillService.getSeckillList();
+        logger.info("list={}",list);
+    }
+
+    @Test
+    public void getById() {
+        Seckill seckill = seckillService.getById(1000);
+        logger.info("seckill={}",seckill);
+    }
+
+    @Test
+    public void exportSeckillUrl() {
+        Exposer exposer = seckillService.exportSeckillUrl(1000);
+        logger.info("exposer={}",exposer);
+    }
+
+    @Test
+    public void excuteSeckill() {
+        try {
+            SeckillExecution result = seckillService.excuteSeckill(1000,1234567800,"6ec34c4dd0c2005b7899d275b72e2eec");
+            logger.info("result={}",result);
+        }catch (RepeatKillException e){
+            logger.error(e.getMessage(),e);
+        }catch (SeckillCloseException e){
+            logger.error(e.getMessage(),e);
+        }
+    }
+}
